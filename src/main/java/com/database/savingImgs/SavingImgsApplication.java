50251dcc7d6e9c39176bcba80e5ef66d37afb6ad
Hassan_ALi <hassan.ali@dailypay.com>
2023-11-08 15:05:24 +0000

    my first commity

---
+++ src/main/java/com/database/savingImgs/SavingImgsApplication.java
@@ -0,0 +1,13 @@
+package com.database.savingImgs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SavingImgsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SavingImgsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/database/savingImgs/SavingImgsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.database.savingImgs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SavingImgsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
