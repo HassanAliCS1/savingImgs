8cfdfb0cb1e9719be3d76b2a10ce4b031782c278
Hassan_ALi <hassan.ali@dailypay.com>
2023-11-09 11:06:45 +0000

    update

---
+++ src/main/java/com/database/savingImgs/entity/ImageData.java
@@ -0,0 +1,29 @@
+package com.database.savingImgs.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.net.ProtocolFamily;
+
+@Entity
+@Table(name="ImageData")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ImageData {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String name;
+    private String type;
+    @Lob
+    @Column(name = "imagedata",length = 1000)
+    private byte[] imageData;
+
+
+}
